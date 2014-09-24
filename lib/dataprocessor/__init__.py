5571808a5e711584bb35990a0add8ad75d90b499
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-09-25 04:22:14 +0900

    Fix __import__

--- lib/dataprocessor/__init__.py
+++ lib/dataprocessor/__init__.py
@@ -8,5 +8,3 @@ from . import pipes
 __all__ = [os.path.basename(f)[:-3]
            for f in glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
            if os.path.basename(f) != "__init__.py"]
-
-mod_list = [__import__(mod, globals(), locals(), [], - 1) for mod in __all__]
--- lib/dataprocessor/pipes/__init__.py
+++ lib/dataprocessor/pipes/__init__.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 """@pipes
 """
+import sys
 import glob
 import os.path
 
@@ -9,7 +10,10 @@ __all__ = [os.path.basename(f)[:-3]
            for f in glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
            if os.path.basename(f) != "__init__.py"]
 
-mod_list = [__import__(mod, globals(), locals(), [], - 1) for mod in __all__]
+for mod in __all__:
+    __import__(__package__ + "." + mod)
+
+mod_list = [sys.modules[__package__ + "." + mod] for mod in __all__]
 
 pipes_dics = {}
 for mod in mod_list:
