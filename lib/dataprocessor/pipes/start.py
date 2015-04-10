b90067a54c367f353233fd2300bbe70e27a27aff
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-04-11 03:29:35 +0900

    Add pipe to start run

---
+++ lib/dataprocessor/pipes/start.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from .. import starter, utility, runner
+
+
+def start(node_list, args, requirements=[],
+          name=utility.now_str(), projects=[],
+          runner="sync", host=None):
+    starter.start(node_list, args, requirements, name, projects, runner, host)
+    return node_list
+
+
+def register(pipes_dics):
+    pipes_dics["start_run"] = {
+        "func": start,
+        "args": [("args", {"nargs": "+", "help": "arguments to execute"})],
+        "kwds": [
+            ("requirements", {"help": "section parameters are written"}),
+            ("name", {"help": "name of new run"}),
+            ("projects", {"nargs": "+", "help": "projects of new run"}),
+            ("runner", {"choices": runner.runners, "help": "runner"}),
+            ("host", {"help": "host in which run is executed"}),
+        ],
+        "desc": "Start new run",
+    }
