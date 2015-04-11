0b19a94f320aa1d0d773ab3e3ec0c35c88bcd12a
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-04-11 15:48:34 +0900

    Bug fix

--- lib/dataprocessor/runner.py
+++ lib/dataprocessor/runner.py
@@ -3,6 +3,7 @@
 """ Start run in any host
 """
 
+from . import utility
 from .exception import DataProcessorError
 from subprocess import check_call, CalledProcessError
 
@@ -22,13 +23,18 @@ class DataProcessorRunnerError(DataProcessorError):
 
 
 def sync(args, work_dir, host=None):
-    args = ["cd", work_dir, "&&"] + args
+    def _check_call(a):
+        try:
+            check_call(a)
+        except CalledProcessError as e:
+            raise DataProcessorRunnerError("sync", args, work_dir, host, exception=e)
+
     if host:
-        args = ["ssh", host] + args
-    try:
-        check_call(args)
-    except CalledProcessError as e:
-        raise DataProcessorRunnerError("sync", args, work_dir, host, exception=e)
+        args = ["ssh", host, "cd", work_dir, "&&"] + args
+        _check_call(args)
+    else:
+        with utility.chdir(work_dir):
+            _check_call(args)
 
 
 runners = {
