a2519be966c31472e9ebec65d579c9900deecd60
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-04-12 20:24:22 +0900

    Add "dispatch" pipe

--- lib/dataprocessor/pipes/start.py
+++ lib/dataprocessor/pipes/start.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
-from .. import starter, utility, runner
+from .. import starter, utility
+from ..runner import runners
 
 
 def start(node_list, args, requirements=[],
@@ -10,16 +11,47 @@ def start(node_list, args, requirements=[],
     return node_list
 
 
+def dispatch(node_list, host, args, requirements=[],
+             name=utility.now_str(), projects=[]):
+    runner = "sync_local"
+    starter.start(node_list, args, requirements, name, projects, runner, host)
+    return node_list
+
+
 def register(pipes_dics):
-    pipes_dics["start_run"] = {
+    args = ("args", {
+        "nargs": "+",
+        "help": "arguments to execute"
+    })
+    requirements = ("requirements", {
+        "nargs": "+",
+        "help": "section parameters are written"
+    })
+    name = ("name", {
+        "help": "name of new run"
+    })
+    projects = ("projects", {
+        "nargs": "+",
+        "help": "projects of new run"
+    })
+    runner = ("runner", {
+        "choices": runners,
+        "help": "runner"
+    })
+    host = ("host", {
+        "help": "host in which run is executed"
+    })
+
+    pipes_dics["starter"] = {
         "func": start,
-        "args": [("args", {"nargs": "+", "help": "arguments to execute"})],
-        "kwds": [
-            ("requirements", {"nargs": "+", "help": "section parameters are written"}),
-            ("name", {"help": "name of new run"}),
-            ("projects", {"nargs": "+", "help": "projects of new run"}),
-            ("runner", {"choices": runner.runners, "help": "runner"}),
-            ("host", {"help": "host in which run is executed"}),
-        ],
+        "args": [args],
+        "kwds": [requirements, name, projects, runner, host],
         "desc": "Start new run",
+    }
+
+    pipes_dics["dispatch"] = {
+        "func": start,
+        "args": [host, args],
+        "kwds": [requirements, name, projects],
+        "desc": "Start new run in remote host",
     }
