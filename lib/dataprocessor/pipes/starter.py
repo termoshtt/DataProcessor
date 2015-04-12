2f187e1e3d94e4aa79efc48408a69be3810e1fa0
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-04-12 20:29:53 +0900

    Add fetch pipe

--- lib/dataprocessor/pipes/starter.py
+++ lib/dataprocessor/pipes/starter.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from .. import starter, utility
+from .. import starter, utility, pipe
 from ..runner import runners
 
 
@@ -16,6 +16,17 @@ def dispatch(node_list, host, args, requirements=[],
     runner = "sync_local"
     starter.start(node_list, args, requirements, name, projects, runner, host)
     return node_list
+
+
+@pipe.type("run")
+def fetch(node):
+    if "host" not in node or "work_dir" not in node:
+        return node
+    host = node["host"]
+    wpath = node["work_dir"]
+    path = node["path"]
+    utility.check_call(["rsync", "-auv", "{host}:{wpath}/".format(host=host, wpath=wpath), path])
+    return node
 
 
 def register(pipes_dics):
@@ -54,4 +65,10 @@ def register(pipes_dics):
         "args": [host, args],
         "kwds": [requirements, name, projects],
         "desc": "Start new run in remote host",
+    }
+    pipes_dics["fetch"] = {
+        "func": fetch,
+        "args": [],
+        "kwds": [],
+        "desc": "Fetch results of dispatched runs"
     }
